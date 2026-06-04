2007910c04c4f4a2361eed9f18f66a9cce568451
rreifel <ricreifel@gmail.com>
2026-06-04 17:47:01 -0600

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  basePath: '/crm',
+  /* This tells the application it is securely nested inside the /crm folder path */
+};
+
+export default nextConfig;
