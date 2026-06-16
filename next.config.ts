703960b9211aa233a545d5cbf3e1c89ddef91738
rreifel <ricreifel@gmail.com>
2026-06-16 14:47:39 -0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/crm',
   reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  }
 };
 
 export default nextConfig;
